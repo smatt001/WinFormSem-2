2c1fa893669a555ab5d5e2056e092eaa00e6c2ad
smatt001 <161397564+smatt001@users.noreply.github.com>
2024-03-20 15:24:20 +0700

    Class Materi Week 05
    Button Creation through code and buttons to remove and create more buttons

---
+++ ClassMateriWeek05/ClassMateriWeek05/Form1.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace ClassMateriWeek05
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tb_button_number = new System.Windows.Forms.TextBox();
+            this.tb_Remove_Button = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // tb_button_number
+            // 
+            this.tb_button_number.Location = new System.Drawing.Point(53, 49);
+            this.tb_button_number.Name = "tb_button_number";
+            this.tb_button_number.Size = new System.Drawing.Size(143, 31);
+            this.tb_button_number.TabIndex = 1;
+            // 
+            // tb_Remove_Button
+            // 
+            this.tb_Remove_Button.Location = new System.Drawing.Point(53, 291);
+            this.tb_Remove_Button.Name = "tb_Remove_Button";
+            this.tb_Remove_Button.Size = new System.Drawing.Size(143, 31);
+            this.tb_Remove_Button.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1502, 735);
+            this.Controls.Add(this.tb_Remove_Button);
+            this.Controls.Add(this.tb_button_number);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tb_button_number;
+        private System.Windows.Forms.TextBox tb_Remove_Button;
+    }
+}
+
---
+++ ClassMateriWeek05/ClassMateriWeek05/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClassMateriWeek05
+{
+    public partial class Form1 : Form
+    {
+        int x = 250;
+        int y = 100;
+        List<Button> list_btn = new List<Button>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            Button btn_show = new Button();
+            btn_show.Name = "Hello";
+            btn_show.Visible = true;
+            btn_show.Text = "Add";
+            btn_show.Size = new System.Drawing.Size(120, 55);
+            btn_show.BackColor = System.Drawing.Color.Black;
+            btn_show.Location = new System.Drawing.Point(25, 43);
+            btn_show.ForeColor = System.Drawing.Color.Yellow;
+            btn_show.Click += btn_show_CLick;
+            this.Controls.Add(btn_show);
+
+            Button btn = new Button();
+            btn.Name = "Hello";
+            btn.Visible = true;
+            btn.Text = "Remove";
+            btn.Size = new System.Drawing.Size(120, 55);
+            btn.BackColor = System.Drawing.Color.Black;
+            btn.Location = new System.Drawing.Point(25, 170);
+            btn.ForeColor = System.Drawing.Color.Yellow;
+            btn.Click += btn_CLick;
+            this.Controls.Add(btn);
+        }
+        private void btn_show_CLick (object sender, EventArgs e)
+        {
+            for (int i = 0; i < Convert.ToInt32(tb_button_number.Text); i++)
+            {
+                Button btn_show = new Button();
+                btn_show.Name = "Hello";
+                btn_show.Visible = true;
+                btn_show.Text = "Button Ke-" + i;
+                btn_show.Size = new System.Drawing.Size(100, 45);
+                btn_show.BackColor = System.Drawing.Color.Black;
+                btn_show.Location = new System.Drawing.Point(x, y);
+                btn_show.ForeColor = System.Drawing.Color.Yellow;
+                btn_show.Click += btn_mulitple;
+                this.Controls.Add(btn_show);
+                list_btn.Insert(0, btn_show);
+
+                if (list_btn.Count == 5)
+                {
+                    x += 100;
+                    y = 50;
+                }
+                y += 55;
+            }
+        }
+        
+        private void btn_CLick(object sender, EventArgs e)
+        {
+            if (list_btn.Count> 0)
+            {
+                Button buttonToRemove = list_btn[0];
+                list_btn.Remove(buttonToRemove);
+                this.Controls.Remove(buttonToRemove);
+            }
+        }
+        private void btn_mulitple (object sender, EventArgs e)
+        {
+            MessageBox.Show("hello");
+        }
+
+        
+
+        
+    }
+}

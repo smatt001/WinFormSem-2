c85c9598a688550f590481db663fb526eacc01e8
smatt001 <161397564+smatt001@users.noreply.github.com>
2024-03-26 09:39:32 +0700

    Class Assignment Week 06
    Making Othello/Reversi

---
+++ ClassAssignmentWeek06/ClassAssignmentWeek06/Main.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace ClassAssignmentWeek06
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnl_Start = new System.Windows.Forms.Panel();
+            this.tb_Size = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_Start = new System.Windows.Forms.Button();
+            this.pnl_Main = new System.Windows.Forms.Panel();
+            this.lbl_Merah = new System.Windows.Forms.Label();
+            this.lbl_Biru = new System.Windows.Forms.Label();
+            this.lbl_Blue = new System.Windows.Forms.Label();
+            this.lbl_Red = new System.Windows.Forms.Label();
+            this.pnl_Start.SuspendLayout();
+            this.pnl_Main.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnl_Start
+            // 
+            this.pnl_Start.Controls.Add(this.btn_Start);
+            this.pnl_Start.Controls.Add(this.label1);
+            this.pnl_Start.Controls.Add(this.tb_Size);
+            this.pnl_Start.Location = new System.Drawing.Point(883, 12);
+            this.pnl_Start.Name = "pnl_Start";
+            this.pnl_Start.Size = new System.Drawing.Size(277, 132);
+            this.pnl_Start.TabIndex = 0;
+            // 
+            // tb_Size
+            // 
+            this.tb_Size.Location = new System.Drawing.Point(115, 34);
+            this.tb_Size.Name = "tb_Size";
+            this.tb_Size.Size = new System.Drawing.Size(136, 26);
+            this.tb_Size.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Input Size :";
+            // 
+            // btn_Start
+            // 
+            this.btn_Start.Location = new System.Drawing.Point(115, 66);
+            this.btn_Start.Name = "btn_Start";
+            this.btn_Start.Size = new System.Drawing.Size(112, 40);
+            this.btn_Start.TabIndex = 3;
+            this.btn_Start.Text = "Start";
+            this.btn_Start.UseVisualStyleBackColor = true;
+            this.btn_Start.Click += new System.EventHandler(this.btn_Start_Click);
+            // 
+            // pnl_Main
+            // 
+            this.pnl_Main.Controls.Add(this.lbl_Blue);
+            this.pnl_Main.Controls.Add(this.lbl_Red);
+            this.pnl_Main.Controls.Add(this.lbl_Biru);
+            this.pnl_Main.Controls.Add(this.lbl_Merah);
+            this.pnl_Main.Location = new System.Drawing.Point(12, 12);
+            this.pnl_Main.Name = "pnl_Main";
+            this.pnl_Main.Size = new System.Drawing.Size(865, 435);
+            this.pnl_Main.TabIndex = 2;
+            // 
+            // lbl_Merah
+            // 
+            this.lbl_Merah.AutoSize = true;
+            this.lbl_Merah.Location = new System.Drawing.Point(588, 65);
+            this.lbl_Merah.Name = "lbl_Merah";
+            this.lbl_Merah.Size = new System.Drawing.Size(54, 20);
+            this.lbl_Merah.TabIndex = 0;
+            this.lbl_Merah.Text = "Merah";
+            // 
+            // lbl_Biru
+            // 
+            this.lbl_Biru.AutoSize = true;
+            this.lbl_Biru.Location = new System.Drawing.Point(662, 65);
+            this.lbl_Biru.Name = "lbl_Biru";
+            this.lbl_Biru.Size = new System.Drawing.Size(37, 20);
+            this.lbl_Biru.TabIndex = 1;
+            this.lbl_Biru.Text = "Biru";
+            // 
+            // lbl_Blue
+            // 
+            this.lbl_Blue.AutoSize = true;
+            this.lbl_Blue.Location = new System.Drawing.Point(662, 95);
+            this.lbl_Blue.Name = "lbl_Blue";
+            this.lbl_Blue.Size = new System.Drawing.Size(18, 20);
+            this.lbl_Blue.TabIndex = 3;
+            this.lbl_Blue.Text = "0";
+            // 
+            // lbl_Red
+            // 
+            this.lbl_Red.AutoSize = true;
+            this.lbl_Red.Location = new System.Drawing.Point(588, 95);
+            this.lbl_Red.Name = "lbl_Red";
+            this.lbl_Red.Size = new System.Drawing.Size(18, 20);
+            this.lbl_Red.TabIndex = 2;
+            this.lbl_Red.Text = "0";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1233, 450);
+            this.Controls.Add(this.pnl_Main);
+            this.Controls.Add(this.pnl_Start);
+            this.Name = "Main";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.pnl_Start.ResumeLayout(false);
+            this.pnl_Start.PerformLayout();
+            this.pnl_Main.ResumeLayout(false);
+            this.pnl_Main.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnl_Start;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tb_Size;
+        private System.Windows.Forms.Button btn_Start;
+        private System.Windows.Forms.Panel pnl_Main;
+        private System.Windows.Forms.Label lbl_Blue;
+        private System.Windows.Forms.Label lbl_Red;
+        private System.Windows.Forms.Label lbl_Biru;
+        private System.Windows.Forms.Label lbl_Merah;
+    }
+}
+
---
+++ ClassAssignmentWeek06/ClassAssignmentWeek06/Main.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClassAssignmentWeek06
+{
+    public partial class Main : Form
+    {
+        int x = 8, y = 9;
+        List<Button> list_Btn = new List<Button>();
+        int ukuran = 0;
+        int merah = 0;
+        int biru = 0;
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_Start_Click(object sender, EventArgs e)
+        {
+            int counter = 1;
+            ukuran = Convert.ToInt32(tb_Size.Text);
+            if (ukuran < 3 || ukuran > 9 || ukuran % 2 == 0)
+            {
+                MessageBox.Show("Size cannot be smaller than 3 or bigger than 9 or be an even number");
+            }
+            else
+            {
+                for (int i = 0; i < ukuran ; i++)
+                {
+                    for (int j = 0; j < ukuran ; j++)
+                    {
+                        Button btn_show = new Button();
+                        btn_show.Name = "Play";
+                        btn_show.Tag = counter.ToString();
+                        btn_show.Visible = true;
+                        btn_show.Size = new System.Drawing.Size(40, 40);
+                        btn_show.Location = new System.Drawing.Point(i *40, j*40);
+                        btn_show.Click += btn_game;
+                        list_Btn.Insert(0, btn_show);
+                        counter++;
+                        this.pnl_Main.Controls.Add(btn_show);
+                    }
+                }
+            }
+            
+        }
+        private void btn_game (object sender, EventArgs e)
+        {
+            Button tombol = (Button)sender;
+            if (tombol.BackColor == Color.Red)
+            {
+                tombol.BackColor = Color.Blue;
+            }
+            else
+            {
+                tombol.BackColor = Color.Red;
+            }
+            int cek = Convert.ToInt32(tombol.Tag) - 1;
+            int cek2 = Convert.ToInt32(tombol.Tag) + 1;
+            int cek3 = Convert.ToInt32(tombol.Tag) + ukuran;
+            int cek4 = Convert.ToInt32(tombol.Tag) - ukuran;
+
+            if (Convert.ToInt32(tombol.Tag)%ukuran == 0)
+            {
+                cek2 = 0;
+            }
+
+            if (Convert.ToInt32(tombol.Tag)%ukuran == 1)
+            {
+                cek = 0;
+            }
+
+            foreach(Button a in list_Btn)
+            {
+                if (a.Tag.ToString() == cek.ToString() || a.Tag.ToString() == cek2.ToString() ||
+                    a.Tag.ToString() == cek3.ToString() || a.Tag.ToString() == cek4.ToString())
+                {
+                    if (a.BackColor == Color.Red)
+                    {
+                        a.BackColor = Color.Blue;
+                    }
+                    else if (a.BackColor == Color.Blue)
+                    {
+                        a.BackColor= Color.Red;
+                    }
+                }
+            }
+
+            merah = 0;
+            biru = 0;
+            foreach (Button a in list_Btn)
+            {
+                if (a.BackColor == Color.Red)
+                {
+                    merah++;
+                }
+                if (a.BackColor == Color.Blue)
+                {
+                    biru++;
+                }
+            }
+            if (merah == ukuran * ukuran || biru == ukuran * ukuran)
+            {
+                MessageBox.Show("WINNN");
+            }
+
+            lbl_Blue.Text = biru.ToString();
+            lbl_Red.Text = merah.ToString();
+        }
+    }
+}
